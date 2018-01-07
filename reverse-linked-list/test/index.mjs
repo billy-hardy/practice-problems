1af76d376c2abea2802ddd3fed20e3d7712e7113
Billy Hardy <billy92@gmail.com>
2018-01-06 22:46:03 -0800

    just use node assertions

--- reverse-linked-list/test/index.mjs
+++ reverse-linked-list/test/index.mjs
@@ -2,12 +2,11 @@ import assert from 'assert';
 import {NodeUtils} from "../../linked-list/index.mjs";
 import {reverse} from "../index.mjs";
 
-describe(() => {
-    const testCases = [
-        [1, 2, 3],
-        [1, 2, 3, 4],
-        [1],
-    ]
+const testCases = [
+    [1, 2, 3],
+    [1, 2, 3, 4],
+    [1],
+]
     .map((values) => {
         return {
             input: NodeUtils.asLinkedList(...values),
@@ -15,15 +14,12 @@ describe(() => {
         };
     });
 
-    testCases
-        .forEach(({input, expected}) => {
-            it("should be reversed", () => {
-                const actual = reverse(input);
-                const actualArray = NodeUtils.asArray(actual);
-                assert.equal(actualArray.length, expected.length);
-                for (let i=0; i<actualArray.length; i++) {
-                    assert.equal(actualArray[i], expected[i]);
-                }
-            });
-        });
-});
+testCases
+    .forEach(({input, expected}) => {
+        const actual = reverse(input);
+        const actualArray = NodeUtils.asArray(actual);
+        assert.equal(actualArray.length, expected.length);
+        for (let i=0; i<actualArray.length; i++) {
+            assert.equal(actualArray[i], expected[i]);
+        }
+    });
